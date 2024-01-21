af402ac439a12ba939052c71a39876b0fa6c7c5c
298671 <157284253+298671@users.noreply.github.com>
2024-01-21 18:04:43 +0300

    Update FINAL_FFT_FILTERED.m

--- FINAL_FFT_FILTERED.m
+++ FINAL_FFT_FILTERED.m
@@ -1,4 +1,4 @@
-% Create a figure window
+
 fig = figure('Position', [100, 100, 600, 400], 'Name', 'Real-time Signal and FFT');
 
 % Create a "Start" button
@@ -14,10 +14,10 @@ function startCallback(~, ~)
     % Prompt user for filter order (n)
     prompt = 'Enter the filter order (n):';
     dlgTitle = 'Filter Order';
-    defaultAnswer = ''; % Default value
+    defaultAnswer = ''; 
     n = str2double(inputdlg(prompt, dlgTitle, [1 40], {defaultAnswer}));
     
-    % Call your existing code with the user-defined filter order
+   
     runYourCode(n);
 end
 
@@ -25,23 +25,23 @@ end
 function stopCallback(~, ~)
     disp('Stop button clicked!');
     
-    % Close the figure
+    
     delete(gcf); % Use gcf to refer to the current figure
 end
 
-% Your existing code function with a modified function signature
+
 function runYourCode(n)
-    % Butterworth filter design using user-defined filter order (n)
-    Wn = 0.6; % Normalized cutoff frequency
+    % Butterworth filter design 
+    Wn = 0.6;
     [b, a] = butter(n, Wn);
 
-    % Arduino setup
-    arduinoPort = 'COM6'; % Change this to your Arduino port
+   
+    arduinoPort = 'COM6'; 
     arduino = serial(arduinoPort, 'BaudRate', 4800);
 
-    % Voltage range of the sensor
-    voltageRange = 10; % Adjust this based on your sensor specifications
-
+   
+    voltageRange = 10; 
+    
     % Set up plot
     subplot(2,1,1);
     hLine = plot(0, 0,'LineWidth',2);
@@ -57,24 +57,24 @@ function runYourCode(n)
     title('Real-time FFT');
     grid on;
 
-    % Set up serial communication
+    
     fopen(arduino);
 
-    % Set up variables
+    
     bufferSize = 250;
     time = 1:bufferSize;
     data = zeros(1, bufferSize);
 
-    % Main loop for real-time data acquisition
+  
     while ishandle(hLine)
         % Read data from Arduino
-        fprintf(arduino, 'R'); % Request data from Arduino
-        rawData = fscanf(arduino, '%d'); % Read the incoming data
+        fprintf(arduino, 'R'); 
+        rawData = fscanf(arduino, '%d'); 
 
-        % Convert raw data to voltage
+      
         voltageData = (rawData / 1023) * voltageRange;
 
-        % Update data buffer
+      
         data = [data(2:end), voltageData];
 
         % Apply Butterworth filter
@@ -85,10 +85,10 @@ function runYourCode(n)
         set(hLine, 'XData', time, 'YData', filteredData);
         ylim([0, voltageRange]);
 
-        % Compute and plot real-time FFT directly on the acquired data
+        % plot real-time FFT 
         fftData = fft(filteredData);
         fftData = abs(fftData(1:bufferSize/2));
-        frequency = (0:(bufferSize/2)-1) * (1500/(bufferSize/2)); % Assuming a sampling rate of 4800 Hz
+        frequency = (0:(bufferSize/2)-1) * (1500/(bufferSize/2)); 
         subplot(2,1,2);
         set(hFFTLine, 'XData', frequency, 'YData', fftData);
         customYLim = [0, max(fftData) * 0.2];
@@ -97,7 +97,7 @@ function runYourCode(n)
         drawnow;
     end
 
-    % Close serial connection
+  
     fclose(arduino);
     delete(arduino);
     clear arduino;
